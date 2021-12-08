36de822e4b5d3544b8b6dbf4a7ebd20cbd03599e
Triana Cerda <trianaRcerda@gmail.com>
2021-12-08 12:18:46 -0800

    added new service files for manager deliverable

--- src/services/players.js
+++ src/services/players.js
@@ -9,3 +9,18 @@ export async function getPlayerById(id) {
   const request = await client.from('players').select('*, teams (*)').match({ id }).single();
   return parseData(request);
 }
+
+export async function updatePlayerById(id, { name, position }) {
+  const request = await client.from('players').update({ name, position }).match({ id });
+  return parseData(request);
+}
+
+export async function createPlayer({ name, position, teamId }) {
+  const request = await client.from('players').insert([{ name, position, team_id: teamId }]);
+  return parseData(request);
+}
+
+export async function deletePlayerById(id) {
+  const request = await client.from('players').delete().match({ id });
+  return parseData(request);
+}
--- src/services/teams.js
+++ src/services/teams.js
@@ -9,3 +9,18 @@ export async function getTeamById(id) {
   const request = await client.from('teams').select('*, players (*)').match({ id }).single();
   return parseData(request);
 }
+
+export async function updateTeamById(id, { name, city, state }) {
+  const request = await client.from('teams').update({ name, city, state }).match({ id });
+  return parseData(request);
+}
+
+export async function createTeam({ name, city, state }) {
+  const request = await client.from('teams').insert({ name, city, state });
+  return parseData(request);
+}
+
+export async function deleteTeamById(id) {
+  const request = await client.from('teams').delete().match({ id });
+  return parseData(request);
+}
