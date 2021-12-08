143bd6bece88ce23132256cebd8726b136381920
Triana Cerda <trianaRcerda@gmail.com>
2021-12-07 16:45:00 -0800

    added TeamList file with useEffect

---
+++ src/views/Teams/TeamList.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getTeams } from '../../services/teams';
+
+function TeamList() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    getTeams().then((resp) => setTeams(resp));
+  }, []);
+  return (
+    <>
+      <h1>List of Teams:</h1>
+      <ul>
+        {teams.map((team) => {
+          return (
+            <li key={team.id}>
+              <Link to={`/teams/${team.id}`} className="team-list-link">
+                {team.name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+}
+
+export default TeamList;
